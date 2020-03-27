b4670fee5808c227870e46a70e1eedc317215aec
snjsomnath <53001297+snjsomnath@users.noreply.github.com>
2020-03-27 20:02:21 +0100

    Update Test.py

---
+++ Test.py
@@ -0,0 +1,22 @@
+#Global inputs
+startingCapital = ""
+gameDuration = ""
+startingRegion = ""
+
+#Game regions
+region_mena =
+region_europe = 
+region_asia = 
+region_australia =
+
+
+
+
+
+
+startingCapital = input("Enter Starting Capital:")
+print("Congratualtions! You start with a capital of: " + startingCapital)
+
+gameDuration = input("How many quarterly rounds would you like to play?")
+print("Great! Just a few more questions")
+print (startingCapital)
